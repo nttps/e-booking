8977829df45b8d2cc5f49d0d858976d5aec18de8
NTTPS <teamerze@gmail.com>
2023-11-30 11:54:40 +0700

    deleate menu

---
+++ components/Settings/Menu.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul class="">
+        
+    </ul>
+</template>
+
+<script setup>
+const authStore = useAuthStore()
+
+
+const isAdmin = computed(() => authStore.user.userInGroups.some(g => g.userGroupID === "ADMIN"))
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
