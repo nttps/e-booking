1e51361fc5ab8c3585ccbdee441b7def8a5aec37
NTTPS <teamerze@gmail.com>
2023-11-30 11:45:53 +0700

    init design

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="flex w-full justify-between px-2 lg:py-4 shadow-md">
+    <header class="flex w-full justify-between p-2 shadow-md">
         <nav
             class="
             flex
@@ -8,9 +8,6 @@
             2xl:gap-x-0
             justify-between
             w-full
-            py-4
-            md:py-0
-            2xl:px-4
             text-lg text-gray-700
             bg-white
             "
@@ -18,70 +15,14 @@
             <div>
                 <a href="#" class="flex justify-center items-center">
                     <img src="~/assets/images/logo.png" class="w-[70px]" alt="E-Learning Logo">
-                    <div class="text-lg 2xl:text-2xl ml-4 font-semibold">
-                        <div>DDPM</div> 
-                        <div class="text-[#FFA133]">E-Learning</div> 
+                    <div class="text-lg 2xl:text-xl ml-4 font-semibold">
+                        <div>ระบบงานดิจิทัล</div> 
+                        <div class="text-[#FFA133]">กรมป้องกันและบรรเทาสาธารณภัย</div> 
                     </div>
                 </a>
             </div>
         
         
-            <div class="hidden w-full md:flex md:items-center md:w-auto" id="menu">
-                <ul
-                    class="
-                    text-base text-gray-700
-                    lg:flex
-                    lg:justify-between 
-                    items-center
-                    space-x-4"
-                >
-                    <li>
-                        <ELink class="nav-header-li" to="/">หน้าแรก</ELink>
-                    </li>
-                    <li>
-                        <ELink class="nav-header-li" to="/course-types">ประเภทหลักสูตร</ELink>
-                    </li>
-                    <li>
-                        <ELink class="nav-header-li" to="/subjects">รายวิชา</ELink>
-                    </li>
-                    <li>
-                        <ELink class="nav-header-li" to="/qa">คำถามที่พบบ่อย</ELink>
-                    </li>
-                    <li>
-                        <ELink class="nav-header-li" to="/tutorial">คู่มือการใช้งาน</ELink>
-                    </li>
-                </ul>
-                <div class="ml-8 hidden xl:block relative ">
-                    <input type="text" v-model="searchText" @paste="search" @input="search" class="input-search" placeholder="ค้นหารายวิชาหรือหลักสูตร" ref="searchBar">
-                    <ul
-                        v-if="searchText.length > 0 || searchSubjects.length"
-                        class="w-full rounded bg-white border border-gray-300 space-y-1 absolute z-10 text-sm mt-1"
-                    >
-                        <li class="px-1 py-2  font-bold border-b border-gray-200" v-if="searchText.length > 0 && searchText.length < 3">
-                            พิมพ์คำค้นหามากกว่า 3 ตัวอักษร
-                        </li>
-                        <li v-if="searchSubjects.length" class="px-2 pt-1 font-bold text-lg">วิชา</li>
-                        <li
-                            v-if="searchSubjects.length"
-                            v-for="subject in searchSubjects"
-                            :key="subject.subject_name"
-                            @click="toSearch(`/subjects/${subject.subject_id}`)"
-                            class="cursor-pointer hover:bg-gray-100 px-4 py-1 truncate"
-                        >
-                        {{ subject.subject_name }}
-                        </li>
-                        <li v-if="searchCourses.length" class="px-2 pt-1 font-bold text-lg">หลักสูตร</li>
-                        <li
-                            v-for="course in searchCourses"
-                            :key="course.course_name"
-                            @click="toSearch(`/course/${course.course_id}`)"
-                            class="cursor-pointer hover:bg-gray-100 px-4 py-1 truncate"
-                        >
-                            {{ course.course_name }}
-                        </li>
-                    </ul>
-                </div>
-            </div>
             <div class="flex relative">
                 <button type="button" class="relative" @click="notificationBar = true" ref="buttonNotificationRef">
                     <img :src="`${notifications.length > 0 ? `/images/notification.svg`: `/images/no-notification.svg`}`" class="w-[50px]" alt="">
@@ -105,26 +46,62 @@
         
     </header>
 
-    <div class="bg-[#494949] lg:pb-16 mb-10">
-        <div class="md:flex justify-between w-4/5 pt-8 pb-16 mx-auto md:px-10 lg:px-14">
-            <div class="md:w-1/2 mb-5 md:mb-0">
-                <h2 class="text-lg md:text-xl lg:text-3xl xl:text-4xl text-white flex font-thin">จองห้องประชุม</h2>
+    <div class="bg-black/50 mb-10 h-64 md:h-96 relative">
+        <div class="absolute top-0 right-0 block w-full h-full">
+            <img
+            alt="Snowy mountain lake"
+            class="object-cover min-w-full h-full"
+            src="~/assets/images/bg-header.jpg"
+            />
+        </div>
+        <div class="z-30 flex flex-col justify-around absolute w-full h-full py-4 lg:py-16 ">
+            <h2 class="text-xl md:text-2xl lg:text-4xl xl:text-6xl text-white font-bold text-center mb-4">จองห้องประชุม</h2>
+            <div class="grid grid-cols-3 gap-x-10 px-4 lg:px-20">
+                <NuxtLink 
+                    to="/" 
+                    v-slot="{ isActive }" 
+                >
+                    <div 
+                        class="rounded-lg border-amber-500 border-2 py-2 px-4  text-center text-2xl font-bold" 
+                        :class="{ 'bg-amber-500': isActive, 'text-white': !isActive}"
+                    >
+                        จองห้องประชุม
+                    </div>
+                    
+                </NuxtLink>
+                <NuxtLink 
+                    to="/booking-list" 
+                    v-slot="{ isActive }" 
+                >
+                    <div 
+                        class="rounded-lg border-amber-500 border-2 py-2 px-4  text-center text-2xl font-bold" 
+                        :class="{ 'bg-amber-500': isActive, 'text-white': !isActive}"
+                    >
+                        รายการจอง
+                    </div>
+
+                    
+                </NuxtLink>
+                <NuxtLink 
+                    to="/calendar" 
+                    v-slot="{ isActive }" 
+                >
+                    <div 
+                        class="rounded-lg border-amber-500 border-2 py-2 px-4  text-center text-2xl font-bold" 
+                        :class="{ 'bg-amber-500': isActive, 'text-white': !isActive}"
+                    >
+                        ปฎิทิน
+                    </div>
+                </NuxtLink>
             </div>
         </div>
+       
+           
     </div>
 </template>
 
 <style>
-    .input-search {
-        padding-left: 1.1rem;
-        padding-top: .25rem;
-        padding-bottom: .25rem;
-        @apply focus-visible:border-gray-400 outline-none rounded-[20px] border border-[#929292] flex items-center text-sm;
-
-    }
-    .nav-header-li {
-        @apply px-2 2xl:px-8 py-2 block hover:bg-[#FFA800] hover:text-white rounded-[20px] transition delay-100;
-    }
+   
 </style>
 
 <script setup>
@@ -149,7 +126,6 @@
     )
 
     onMounted(() => {
-        fetchNotification();
     });
 
 
@@ -158,84 +134,10 @@
         notificationBar.value = false
     }
 
-    const searchSubjects = ref([])
-    const searchCourses = ref([])
-
-    
-
-    const search = (e) =>{
-
-        if (searchText.value === '') {
-          return searchSubjects.value = []
-        }
-
-        if (searchText.value.length < 3) {
-          return
-        }
-
-        const subjectUrl = config.public.apiUrl + config.public.api.subject.lists;
-
-        var raw = JSON.stringify({
-            "SearchText": e ? e.target.value : "",
-            "Status": "เปิด",
-            "Cate":"",//subject cate (หมวดวิชา)
-            "Group":"",//subject group (กลุ่มวิชา)
-            "IsAdmin": "0"
-        });
-        var requestOptions = {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json; charset=UTF-8",
-            },
-            body: raw,
-        };
-        fetch(subjectUrl, requestOptions)
-            .then(response => response.json())
-            .then( json => {
-                searchSubjects.value = json
-            })
-            .catch(error => console.log('error', error));
-
-        const courseUrl = config.public.apiUrl + config.public.api.course.lists;
-
-        var raw = JSON.stringify({
-            "SearchText": e ? e.target.value : "",
-            "Status": "เปิด",
-            "Cate":"",//subject cate (หมวดวิชา)
-            "Group":"",//subject group (กลุ่มวิชา)
-            "IsAdmin": "0"
-        });
-        var requestOptions = {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json; charset=UTF-8",
-            },
-            body: raw,
-        };
-        fetch(courseUrl, requestOptions)
-            .then(response => response.json())
-            .then( json => {
-                searchCourses.value = json
-            })
-            .catch(error => console.log('error', error));
-
-    }
-
     const notifications = ref([])
 
     const fetchNotification = async () => {
-        const url = config.public.apiUrl + config.public.api.notification.lists;
-
-        const res = await fetch(`${url}?username=${user.user.currentUser}&show_unread_only=y`)
-        const data = await res.json()
-
-        notifications.value = data
+        notifications.value = []
     }
 
-    const toSearch = (url) => {
-        navigateTo(url)
-
-        searchText.value = ''
-        searchSubjects.value = []
-    }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,4 @@
 <template>
-    <div class="bg-[#494949] lg:pb-16 mb-10">
-        <div class="md:flex justify-between w-4/5 pt-8 pb-16 mx-auto md:px-10 lg:px-14">
-            <div class="md:w-1/2 mb-5 md:mb-0">
-                <h2 class="text-lg md:text-xl lg:text-3xl xl:text-4xl text-white flex font-thin">จองห้องประชุม</h2>
-            </div>
-        </div>
-    </div>
-
- 
 </template>
 
 
@@ -17,148 +8,17 @@
         middleware: 'auth'
     })
 
-    const config = useRuntimeConfig();
-    const recommandCourses = ref([])
-    const courses = ref([])
-    const subjects = ref([])
-    const authStore = useAuthStore()
-
-
 
     onMounted(() => {
-        fetchDataCourse();
-        fetchDataRecommand()
-        fetchDataSubject()
+      
 
     });
 
-    const myHeaders = new Headers();
-    const url = config.public.apiUrl + config.public.api.course.lists;
-
-
-    myHeaders.append("Content-Type", "application/json");
-
-    const raw = JSON.stringify({
-        "SearchText": "",
-        "Status": "",
-        "Status": "เปิด",
-        "LitmitRow": "10",
-        "Type":"",//course type(กลุ่มวิชา)
-        "Cate":"",//course cate (หมวดวิชา)
-        "Group":"",//course group (กลุ่มหมวดวิชา)
-        "Random": 1,
-        "user": authStore.user.currentUser,//current user login
-    });
-
-    const raw2 = JSON.stringify({
-        "SearchText": "",
-        "Status": "",
-        "Status": "เปิด",
-        "LitmitRow": "10",
-        "Type":"",//course type(กลุ่มวิชา)
-        "Cate":"",//subject cate (หมวดวิชา)
-        "Group":"",//subject group (กลุ่มวิชา)
-        "Random": 1,
-        "user": authStore.user.currentUser,//current user login
-    });
-
-    const fetchDataCourse = () => {
-        fetch(url, {
-            method: 'POST',
-            body: raw,
-            headers: myHeaders,
-        })
-            .then(response => response.json())
-            .then( json => {
-                courses.value = json
-            })
-        .catch(error => console.log('error', error));
-    }
-    const fetchDataRecommand = () => {
-    
-        fetch(url, {
-            method: 'POST',
-            body: raw2,
-            headers: myHeaders,
-        })
-            .then(response => response.json())
-            .then( json => {
-                recommandCourses.value = json
-            })
-        .catch(error => console.log('error', error));
-    }
-
-    const fetchDataSubject = () => {
-        const url = config.public.apiUrl + config.public.api.subject.lists;
-
-        fetch(url, {
-            method: 'POST',
-            body: raw,
-            headers: myHeaders,
-        })
-            .then(response => response.json())
-            .then( json => {
-                subjects.value = json
-            })
-        .catch(error => console.log('error', error));
-    }
-
+   
 </script>
 
 
 
 <style lang="scss">
-    .search-form {
-        @apply flex flex-col items-center mb-4 md:mb-20;
-    }
 
-    .input-form {
-    }
-    .input-form input {
-        @apply border rounded p-1.5 md:p-2.5 outline-none md:w-[514px] text-xl;
-    }
-    
-
-    .course-item {
-        box-shadow: 0px 4px 4px 0px #00000040;
-    }
-
-
-    $width: 100%;
-
-    @mixin half-circle ($direction: right) {
-        @if $direction == right {
-            border-radius: 0 100% 100% 0 / 0 50% 50% 0;
-        }
-        @else if $direction == left {
-            border-radius: 100% 0 0 100% / 50% 0 0 50%;
-        }
-        @else if $direction == top {
-            border-radius: 50% 50% 0 0 / 100% 100% 0 0;
-        }
-        @else if $direction == bottom {
-            border-radius: 0 0 50% 50% / 0 0 100% 100%;
-        }
-        @else {
-            @error 'Please set correct $direction value.'
-        }
-    }
-
-    .half-circle {
-        background-color: #FFA133;
-        &.horizontal {
-            width: $width;
-        }
-
-        @media (min-width: 768px) {
-            &.horizontal {
-                font-size: 1rem/* 16px */;
-                border-radius: 0 0 50% 50% / 0 0 100% 100%;
-            }
-        }
-    }
-
-    .page-item {
-        @apply w-12 border border-[#959595] md:flex justify-center items-center hidden text-2xl  cursor-pointer transition duration-150 ease-in hover:bg-[#FFA133] hover:text-white rounded-[4px];
-    }
 </style>
