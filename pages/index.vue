933104e902030628c5ed440721409215c80c146c
NTTPS <teamerze@gmail.com>
2024-01-22 11:34:55 +0700

    hide status room

--- pages/index.vue
+++ pages/index.vue
@@ -61,9 +61,9 @@
                     <div class="flex items-center space-x-2 mb-2 text-gray-500 text-lg">
                         <span>หน่วยงาน </span><span>{{ room.department_id }}</span>
                     </div>
-                    <div class="flex items-center space-x-2 mb-2 text-gray-500 text-lg">
+                    <!-- <div class="flex items-center space-x-2 mb-2 text-gray-500 text-lg">
                         <span>สถานะ</span> <span class="rounded-full px-4 text-black" :class="{ 'bg-green-400': room.status === 'ปกติ', 'bg-red-400': room.status !== 'ปกติ'}">{{ room.status }}</span>
-                    </div>
+                    </div> -->
                     
                 </div>
                 <div class="w-1/2 self-end">
